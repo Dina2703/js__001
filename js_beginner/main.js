f25732c645545496b06005e429b502db8bae4019
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-04-17 13:14:12 -0400

    basic

---
+++ js_beginner/main.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+const person = {
+  name: "Anna",
+  age: 27,
+  isMarried: false,
+};
+
+console.log(person.age);
+
+//ternary operator example
+const isEven = 10 % 5 === 0 ? true : false;
+console.log(isEven);
