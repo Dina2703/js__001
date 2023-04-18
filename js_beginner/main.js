e9255e328dbd1edeaac388da12a15355598b85de
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-04-17 21:55:48 -0400

    Conditional Statement and Looping Code

--- js_beginner/main.js
+++ js_beginner/main.js
@@ -55,3 +55,51 @@ console.log(Boolean(null)); //false
 console.log(Boolean(undefined)); //false
 console.log(Boolean("")); //false
 console.log(Boolean(NaN)); //false
+
+//equality
+// const var1 = "";
+// const var2 = 0;
+
+// console.log(var1 == var2);
+// console.log(var1 === var2);
+
+//------------Conditional Statement---------
+
+const num = 0;
+
+if (num > 0) {
+  console.log("The number is positive");
+} else if (num < 0) {
+  console.log("The number is negative");
+} else {
+  console.log("The number is 0");
+}
+
+//------------Looping Code---------
+//---For Loop----
+for (let i = 1; i <= 5; i++) {
+  console.log("Iteration number " + i);
+}
+
+//---While Loop----
+let j = 1;
+while (j <= 5) {
+  console.log("Iteration number " + j);
+  j++;
+}
+
+//---Do...While Loop----
+
+let l = 7;
+do {
+  console.log("Iteration number " + l);
+  l++;
+} while (l <= 5);
+
+//---For...of Loop----
+
+const numArray = [1, 2, 3, 4, 5];
+
+for (const num of numArray) {
+  console.log("Iteration number " + num);
+}
